781d6448431e5a624aff8daf6cd541b8f6ae68e8
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-03-24 16:08:30 -0500

    spacing

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 function init(){
   var button = document.getElementById('entrybutton');
-
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("Zealous Godwin: " + textbox.value);
   }
-
   button.addEventListener('click', showMeText);
   }
-  
 window.addEventListener('load', init);
