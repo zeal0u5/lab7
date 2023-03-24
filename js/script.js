35fa192ffc0bec0d4e9b14544e9585723443120d
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-03-24 16:04:27 -0500

    pls work

--- js/script.js
+++ js/script.js
@@ -1,23 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
-// Write JavaScript code to do the following things based on the demos we did in class.
-// a. Add an event listener to the Alert Me div so that when it is clicked it will run some code.
-// b. When the button is clicked it should do two things.
-// c. (1) It should show an Alert box with the contents of the text field in the Alert box. I want you to also append 
-// your name and a colon
-// (name:) before the value of the text box. For example if the text box says This is a test, I would have the Alert 
-// say Daniel Krieglstein: This is a test
-// d. (2) Also it should change the value of the h2 below the button to have the text contents of the text input 
-// control. Do not append your name
-// to this one. Just have the exact contents of the text box.
-// e. I will show an example of this behavior in class in a final version of the lab.
   var button = document.getElementById('entrybutton');
 
-  function showText() {
+  function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("Zealous Godwin: " + textbox.value);
-}
-  button.addEventListener('onclick', showText);
-}
+  }
+
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
